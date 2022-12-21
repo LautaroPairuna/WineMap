22351a95254cbe4e737c4bdea7221897a77a4749
LautaroPairuna <108547952+LautaroPairuna@users.noreply.github.com>
2022-12-21 20:30:00 -0300

    Cambios 21/12

---
+++ README.md
@@ -0,0 +1,44 @@
+# WineMap
+
+Para poder inicializar el proyecto debera abrir la consola dentro de la carpeta del proyecto y escribir "npm run dev"
+
+Dejo un anexo del repositorio del Dashboard de administracion: https://github.com/LautaroPairuna/dashboardWineMap.git
+
+
+/* -----------------------------------------------------------------------------------------------  
+Herramientas y librerias usadas:
+
+Las herramientas utilizadas son HTML, CSS, JAVASCRIPT, NODEJS, MYSQL
+
+Y las librerias utilizadas son EXPRESS, EXPRESS-SESSION, EXPRESS-VALIDATOR, EJS, BCRYPT, BODY-PARSER, COOKIE-PARSER, SEQUELIZE y MYSQL2
+
+
+/* -----------------------------------------------------------------------------------------------  
+Tematica del Ecommerce
+
+La temática de Carrito Wine Map Salta sera una tienda digital donde se venderan vinos de todas las bodegas de Salta
+
+/* -----------------------------------------------------------------------------------------------  
+Historia de Wine Map Salta
+
+Wine Map Salta es una guía turística en forma de tríptico (la cual ya tiene su propia web https://www.winemapsalta.com.ar/), que incluye un mapa conceptual con la ubicación de todas las bodegas y datos de interés general para el turista y para todos aquellos que sentimos una pasión por la degustación de diversos vinos. Y ahora estamos buscando comercializar los vinos de dichas bodegas a travez de un ecommerce
+
+/* -----------------------------------------------------------------------------------------------  
+Publico Objetivo
+
+El publico objetivo de esta tienda digital sera toda persona que quiera disfrutar de un buen vino salteño
+
+/* -----------------------------------------------------------------------------------------------  
+Paleta de colores 
+
+Utilizaremos colores representativos de Salta como el bordo o el dorado, y lo acompañaremos de colores neutros como el blanco o el negro
+
+/* -----------------------------------------------------------------------------------------------  
+Tipografía
+
+Utilizaremos la Tipografía Roboto para el desarrollo del ecommerce, esta tipografía fue sacada de Google Fonts https://fonts.google.com/specimen/Roboto
+
+/* -----------------------------------------------------------------------------------------------  
+Logo
+
+Por el momento no tenemos un logo desarrollado a medida pero dejaremos un prototipo de lo que sería el logo final (obviamente tendra modificaciones mas a futuro)
--- public/js/addCart.js
+++ public/js/addCart.js
@@ -1,19 +1,121 @@
-const items = document.querySelector('#contenedor_productos')
-let carrito = {}
+document.addEventListener('DOMContentLoaded', () => {  
 
-items.addEventListener('click', (e) => {
-    addCart(e)
-})
+    const compraBtn = document.querySelectorAll('#btn-compra')
+    const tbody = document.querySelectorAll('.tbody')
+    let carrito = []
 
-const addCart = e => {
+    compraBtn.forEach(btn => {
+        btn.addEventListener('click', product)
+    })
 
-    if(e.target.classList.contains('btn-compra')) {
-        setCarrito(e.target.parentElement)
+    function product(e) {
+        const button = e.target
+        const item = button.closest('#contenedor_producto')
+        const itemProducto = item.querySelector('#itemProducto').textContent
+        const itemPrecio = item.querySelector('#itemPrecio').textContent
+        const itemFoto = item.querySelector('#itemFoto').src
+        const ItemCantidad = item.querySelector('#itemCantidad')
+
+        const newItem = {
+            producto: itemProducto,
+            precio: itemPrecio,
+            foto: itemFoto,
+            cantidad: ItemCantidad || 1
+        }
+
+        addToCart(newItem)
+
     }
-    e.stopPropagation()
 
-}
+    function addToCart(newItem){
 
-const setCarrito = objeto => {
-    console.log(objeto)
-}
+        for(let i = 0 ; i < carrito.length ; i++){
+            if(carrito[i].producto.trim() == newItem.producto.trim()){
+                carrito[i].cantidad ++;
+
+                console.log(carrito)
+            }
+        }
+
+        carrito.push(newItem)
+        renderizarCarrito()
+
+    }
+
+    window.addEventListener("resize", renderizarCarrito);
+
+    function renderizarCarrito(){
+
+        if(window.innerWidth > 900){
+
+            tbody.innerHTML = ""
+
+            carrito.map(item => {
+
+                const DOMtr = document.createElement('tr')
+                DOMtr.classList.add('ItemCarrito')
+                const Content = `
+                <td class="flex-container-cart-product">
+                    <img src=${item.foto} alt="" class="img-width">
+                    <div class="flex-container-cart-text">
+                        <h4>${item.producto}</h4>
+                        <p>Precio: $${item.precio}</p>
+                    </div>
+                </td>
+                <td>
+                    <div class="flex-container-cart-product">
+                        <button class="btn-carrito"><img src="public/images/iconos/ico-minun.svg" alt="" class="w-24"></button>
+                        <p class="mx-3">${item.cantidad}</p>
+                        <button class="btn-carrito"><img src="public/images/iconos/ico-plus.svg" alt="" class="w-24"></button>
+                        <button class="btn-carrito"><img src="public/images/iconos/ico-delete.svg" alt="" class="w-24"></button>
+                    </div>
+                </td>
+                <td>
+                    <p>$${item.precio}</p>
+                </td>`
+
+                DOMtr.innerHTML = Content
+
+                tbody.appendChild(DOMtr)
+
+            })
+
+
+        }else{
+
+            tbody.innerHTML = ""
+
+            carrito.map(item => {
+
+                const DOMtr = document.createElement('tr')
+
+                DOMtr.classList.add('ItemCarrito')
+
+                const Content = `
+                <td class="flex-container-cart-product">
+                    <img src=${item.foto} alt="" class="img-width">
+                    <div class="flex-container-cart-text">
+                        <h4>${item.producto}</h4>
+                        <div class="flex-container-cart-product">
+                            <button class="btn-carrito"><img src="public/images/iconos/ico-minun.svg" alt="" class="w-24"></button>
+                            <p class="mx-3">${item.cantidad}</p>
+                            <button class="btn-carrito"><img src="public/images/iconos/ico-plus.svg" alt="" class="w-24"></button>
+                            <button class="btn-carrito"><img src="public/images/iconos/ico-delete.svg" alt="" class="w-24"></button>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <p>$${item.precio}</p>
+                </td>`
+
+                DOMtr.innerHTML = Content;
+
+                tbody[0].appendChild(DOMtr)
+
+            })
+
+        }
+
+    }
+
+})
